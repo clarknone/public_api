10c1d8319cba0c412f17e999ac2a5543ff2cfc20
orion <carlasparsh@gmail.com>
2022-10-10 13:06:40 +0100

    statics

--- api/api/settings.py
+++ api/api/settings.py
@@ -122,6 +122,7 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
